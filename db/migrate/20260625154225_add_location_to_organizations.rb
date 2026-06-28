db80b8d15b7e446cb58483f24a819d452ccede85
naman79820 <naman79820@gmail.com>
2026-06-28 09:30:57 +0000

    chore: revert accidental Gemfile and db changes
    - Revert Gemfile to master (no ruby-lsp-refactor removal, no concurrent-ruby downgrade)
    - Revert Gemfile.lock to master
    - Remove db/migrate/add_location_to_organizations (already exists in feat/add-org-location-db)
    - Revert db/schema.rb to master

--- db/migrate/20260625154225_add_location_to_organizations.rb
+++
@@ -1,5 +0,0 @@
-class AddLocationToOrganizations < ActiveRecord::Migration[8.1]
-  def change
-    add_column :organizations, :location, :string unless column_exists?(:organizations, :location)
-  end
-end
