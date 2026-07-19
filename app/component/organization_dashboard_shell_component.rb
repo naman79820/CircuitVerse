6d4de2cf3685f5e087c733bbd6a3663e3954aad6
naman79820 <naman79820@gmail.com>
2026-07-19 11:11:04 +0000

    chore: switch organization implmentation

--- app/component/organization_dashboard_shell_component.rb
+++ app/component/organization_dashboard_shell_component.rb
@@ -3,10 +3,11 @@
 class OrganizationDashboardShellComponent < ViewComponent::Base
   renders_one :tab_content
 
-  def initialize(organization:, active_tab:)
+  def initialize(organization:, active_tab:, user_organizations:)
     super()
     @organization = organization
     @active_tab = active_tab
+    @user_organizations = user_organizations
   end
 
   def tab_class(tab)
@@ -15,5 +16,13 @@ class OrganizationDashboardShellComponent < ViewComponent::Base
 
   def show_settings_tab?
     helpers.policy(@organization).admin_access?
+  end
+
+  def initials(organization)
+    organization.name.split.pluck(0).first(2).join.upcase
+  end
+
+  def current_org?(organization)
+    organization.id == @organization.id
   end
 end
